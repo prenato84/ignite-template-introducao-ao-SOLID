ff32dfdf6383de6093bcc326a0f0725779f0cd4d
Paulo Castro <prenato84@gmail.com>
2021-03-28 20:34:51 -0300

    :heavy_check_mark: Handle thrown error by show user profile use case

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -8,7 +8,13 @@ class ShowUserProfileController {
   handle(request: Request, response: Response): Response {
     const { user_id } = request.params;
 
-    const user = this.showUserProfileUseCase.execute({ user_id });
+    let user;
+
+    try {
+      user = this.showUserProfileUseCase.execute({ user_id });
+    } catch (error) {
+      return response.status(404).json({ error: `${error.message}` });
+    }
 
     return response.status(200).json(user);
   }
