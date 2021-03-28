10f7894dae0ed45107f8aafb6411a117694bf4a3
Paulo Castro <prenato84@gmail.com>
2021-03-28 19:18:58 -0300

    :sparkles: Turn user admin controller

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,11 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    this.turnUserAdminUseCase.execute({ user_id });
+
+    return response.status(204).send();
   }
 }
 
