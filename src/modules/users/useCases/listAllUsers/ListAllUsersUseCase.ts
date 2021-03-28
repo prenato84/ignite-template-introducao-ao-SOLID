e9d8ecedab052f8806d278df7025b78e0fbdb2e1
Paulo Castro <prenato84@gmail.com>
2021-03-28 20:01:17 -0300

    :sparkles: List all users use case

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,17 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const foundUser = this.usersRepository.findById(user_id);
+
+    if (!foundUser) {
+      throw new Error("User does not exist!");
+    }
+
+    if (!foundUser.admin) {
+      throw new Error("User provided is not an admin!");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
