32af4325cbcb91f9129a919247292ae2992978b5
Paulo Castro <prenato84@gmail.com>
2021-03-28 19:06:18 -0300

    :sparkles: Create user use case

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,16 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const emailAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (emailAlreadyExists) {
+      throw new Error("E-mail address is already taken!");
+    }
+
+    return this.usersRepository.create({
+      name,
+      email,
+    });
   }
 }
 
