d0a13b21f50cec336b236fbade08615431b6ee79
Paulo Castro <prenato84@gmail.com>
2021-03-28 19:09:04 -0300

    :sparkles: Create user controller

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,11 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { name, email } = request.body;
+
+    this.createUserUseCase.execute({ name, email });
+
+    return response.status(201).send();
   }
 }
 
