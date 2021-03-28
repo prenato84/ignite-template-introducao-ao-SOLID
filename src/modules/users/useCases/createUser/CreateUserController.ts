cc328741cacf2dfd1d7ff1a78179653f4576c157
Paulo Castro <prenato84@gmail.com>
2021-03-28 20:30:02 -0300

    :heavy_check_mark: Handle thrown error by create user use case

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -8,7 +8,11 @@ class CreateUserController {
   handle(request: Request, response: Response): Response {
     const { name, email } = request.body;
 
-    this.createUserUseCase.execute({ name, email });
+    try {
+      this.createUserUseCase.execute({ name, email });
+    } catch (error) {
+      return response.status(400).json({ error: `${error.message}` });
+    }
 
     return response.status(201).send();
   }
