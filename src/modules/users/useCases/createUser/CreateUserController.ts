c6c13cab1b0ee8406f06e7462b0b8535cb1d96be
Paulo Castro <prenato84@gmail.com>
2021-03-28 21:07:16 -0300

    :heavy_check_mark: Fix response to return expected json

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -9,8 +9,8 @@ class CreateUserController {
     const { name, email } = request.body;
 
     try {
-      this.createUserUseCase.execute({ name, email });
-      return response.status(201).send();
+      const user = this.createUserUseCase.execute({ name, email });
+      return response.status(201).json(user);
     } catch (error) {
       return response.status(400).json({ error: error.message });
     }
