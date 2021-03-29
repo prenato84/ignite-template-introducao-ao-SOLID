63e899d439b7237209b2e14b4cbb1eec95ca7ceb
Paulo Castro <prenato84@gmail.com>
2021-03-28 21:08:03 -0300

    :sparkles: List all users controller

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,16 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    let users;
+
+    try {
+      users = this.listAllUsersUseCase.execute({ user_id: String(user_id) });
+      return response.status(202).json(users);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
